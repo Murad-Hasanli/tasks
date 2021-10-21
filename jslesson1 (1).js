b2c94c4914ebd765033daebc59890abb486e4afc
Murad kh. Hasanli <muradkhh@code.local>
2021-10-21 18:19:59 +0400

    added

---
+++ jslesson1 (1).js
@@ -0,0 +1,46 @@
+/* arrayin butun elementlerini cap edin */
+/* let number=[1,10,20,30,40,50,60,70]
+console.log(number); */
+
+//  arrayin en boyuk elementini cap edin 
+//  let $number=[20,10,1,30,40,50,70,60]
+
+// console.log(Math.max(...$number));
+// console.log(Math.min(...$number));
+
+
+
+/* arrayin sonuna 15 ededi elave edin */
+/* let _number=[1,10,20,30,40,50,60,70]
+let newNumber=_number.push('15')
+console.log(_number) */
+
+/* arrayin 1ci ededini silin
+//  *//* let _number=[1,10,20,30,40,50,60,70] */
+// let firstNumber=_number.shift()
+// console.log(_number) 
+
+// arrayin sonuncu ededini cap edin
+
+// let $number=[1,10,20,30,40,50,60,70]
+// $number.sort()
+// let lastElement = $number[$number.length - 1];
+// console.log(lastElement)
+
+// foreach ile elementleri cap edin
+// let $number=[1,10,20,30,40,50,60,70]
+// $number.forEach(element => console.log(element));
+
+// arrayin 3cu ededinden sonraki ededleri cap edin
+// let $number=[1,10,20,30,40,50,60,70]
+// for (let i = 3; i < $number.length; i++) {
+//   console.log($number[i]);
+// }
+
+
+// arrayin 40ci elementinden sonraki elementleri cap edin
+// let $number=[1,10,20,30,40,50,60,70]
+//  for (let i = 5; i < $number.length; i++) {
+//    console.log($number[i]);
+//  }
+
